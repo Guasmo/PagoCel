64e93c270dcb073b868bc5271a78334e7b384193
Juan Buri <juancalveti4@gmail.com>
2026-08-05 18:58:34 -0500

    feat(what-is-pagocel): expand ecosystem explanation and visuals

--- src/app/pages/what-is-pagocel/what-is-pagocel.ts
+++ src/app/pages/what-is-pagocel/what-is-pagocel.ts
@@ -3,10 +3,11 @@ import { Definition } from './sections/definition/definition';
 import { MobileIdentifier } from './sections/mobile-identifier/mobile-identifier';
 import { NotABankReplacement } from './sections/not-a-bank-replacement/not-a-bank-replacement';
 import { WhoItConnects } from './sections/who-it-connects/who-it-connects';
+import { DownloadSection } from '../../components/download-section/download-section';
 
 @Component({
   selector: 'app-what-is-pagocel',
-  imports: [Definition, MobileIdentifier, NotABankReplacement, WhoItConnects],
+  imports: [Definition, MobileIdentifier, NotABankReplacement, WhoItConnects, DownloadSection],
   templateUrl: './what-is-pagocel.html',
   styleUrl: './what-is-pagocel.css'
 })

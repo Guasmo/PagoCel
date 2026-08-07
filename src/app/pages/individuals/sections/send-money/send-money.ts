b6638283291d5766a68f8859619cf9633d1a81e2
Juan Buri <juancalveti4@gmail.com>
2026-08-07 18:15:32 -0500

    feat(individuals-send-money): update feature images and banner

--- src/app/pages/individuals/sections/send-money/send-money.ts
+++ src/app/pages/individuals/sections/send-money/send-money.ts
@@ -18,19 +18,19 @@ export class SendMoney {
       num: '01',
       title: 'Envío de dinero entrante',
       description: 'Elige a quién enviar desde tu agenda. Solo necesitas su número de celular, sin pedir números de cuenta.',
-      img: '/mockups/mockup-7.png'
+      img: '/mockups/mockup-8.png'
     },
     {
       num: '02',
       title: 'Recibe dinero',
       description: 'Comparte tu número celular y recibe pagos al instante. Olvídate de dictar números de cuenta largos.',
-      img: '/mockups/mockup-9.png'
+      img: '/mockups/mockup-7.png'
     },
     {
       num: '03',
       title: 'Pago con QR',
       description: 'Paga de forma rápida y segura escaneando el código QR de los comercios afiliados.',
-      img: '/mockups/mockup-8.png'
+      img: '/mockups/mockup-5.png'
     }
   ];
 }
